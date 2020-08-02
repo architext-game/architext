369244f472c56cc980c185fc2329fdd9b9830546
Óliver Sanz <oliverlsanz@gmail.com>
2020-08-02 11:06:52 +0200

    update help verb

--- server/verbs/help.py
+++ server/verbs/help.py
@@ -12,26 +12,30 @@ Estos son las acciones que puedes escribir en el juego:
 
 BÁSICAS
   - "mirar" para mirar a tu alrededor.
-  - "mirar <nombre del objeto>" para mirar un objeto de cerca.
+  - "mirar <nombre del objeto/salida>" para mirar de cerca un objeto o una salida.
   - "ir <salida>" para cruzar una salida.
   - "decir <mensaje>" enviar un mensaje a todos los que estén en tu misma localización.
+  
+AVANZADAS
   - "gritar <mensaje>" para enviar un mensaje a todos.
   - "emote <acción>" para hacer una acción, y que todos los que están contigo lo vean.
+  - "salidas" para ver las salidas de tu habitación. Ojo: puede haber salidas ocultas.
+  - "objetos" para ver los objetos de tu habitación. Ojo: puede haber objetos ocultos.
 
 CONSTRUCCIÓN SENCILLA
   - "construir" para construir una habitación adyacente a donde estás.
   - "fabricar" para crear un objeto en la habitación donde estás.
   - "reformar" para modificar la habitación en la que te encuentras.
-  - "editar <nombre del objeto>" para modificar un objeto de tu habitación.
+  - "editar <nombre del objeto/salida>" para modificar un objeto o salida de tu habitación.
 
 CONSTRUCCIÓN AVANZADA
   - "info" para ver toda la información de la habitación donde estás.
-  - "info <nombre del objeto> para ver la info de un objeto de tu habitación.
+  - "info <nombre del objeto/salida> para ver la info de un objeto o salida de tu habitación.
   - "conectar" para conectar la sala donde estás con otra sala.
       (Necesitarás el alias de la sala, usa el comando "info")
   - "eliminarsala" para borrar la sala donde estás. SIN REMEDIO.
-  - "eliminarsalida <nombre de la salida>" para borrar una salida.
-  - "eliminarobjeto <nombre del objeto>" para borrar un objeto.
+  - "eliminarsalida <nombre de la salida>" para borrar una salida. SIN REMEDIO.
+  - "eliminarobjeto <nombre del objeto>" para borrar un objeto. SIN REMEDIO.
   - "tp <alias de la sala>" para transportarte a cualquier sala.
       (El alias puedes consultarlo con el comando "info")
 """)
