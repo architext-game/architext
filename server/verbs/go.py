afa07c215816aefcf0772834f3237f5b66a903fe
Óliver Sanz <oliverlsanz@gmail.com>
2020-08-06 12:39:57 +0200

    no spoilers in go verb Go verb does not tell others where a user goes or comes from if the exit is hidden.

--- server/verbs/go.py
+++ server/verbs/go.py
@@ -13,7 +13,11 @@ class Go(Verb):
         available_exits = [exit.name for exit in self.session.user.room.exits]
         possible_meanings = util.possible_meanings(partial_exit_name, available_exits)
         if len(possible_meanings) == 1:
-            selected_exit = possible_meanings[0]
+            selected_exit_name = possible_meanings[0]
+            for exit in self.session.user.room.exits:
+                if exit.name == selected_exit_name:
+                    selected_exit = exit
+                    break
             self.go(selected_exit)
         elif len(possible_meanings) > 1:
             self.session.send_to_client('Hay más de una salida con ese nombre. Sé más específico.')
@@ -22,13 +26,20 @@ class Go(Verb):
 
         self.finish_interaction()
 
-    def go(self, exit_name):
+    def go(self, selected_exit):
         origin_room = self.session.user.room
-        self.session.send_to_others_in_room("{} se marcha por {}.".format(self.session.user.name, exit_name))
-        self.session.user.move(exit_name)
-        try:
-            there_exit = [exit.name for exit in self.session.user.room.exits if exit.destination == origin_room][0]
-            self.session.send_to_others_in_room("{} llega desde {}.".format(self.session.user.name, there_exit))
-        except:
+
+        if not selected_exit.hidden():
+            self.session.send_to_others_in_room("{} se marcha por {}.".format(self.session.user.name, selected_exit.name))
+        else:
+            self.session.send_to_others_in_room("{} se marcha hacia algún lugar.".format(self.session.user.name))
+        
+        self.session.user.move(selected_exit.name)
+
+        there_exit = [exit.name for exit in self.session.user.room.exits if exit.destination == origin_room and not exit.hidden()]
+        if there_exit:
+            self.session.send_to_others_in_room("{} llega desde {}.".format(self.session.user.name, there_exit[0]))
+        else:
             self.session.send_to_others_in_room("{} llega desde algún lugar.".format(self.session.user.name))
+
         Look(self.session).show_current_room()
