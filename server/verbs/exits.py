6697f792666b5038bf4d5c8e598c9c03bee49f58
Óliver Sanz <oliverlsanz@gmail.com>
2020-08-06 23:56:14 +0200

    fix exits verb output formatting

--- server/verbs/exits.py
+++ server/verbs/exits.py
@@ -8,7 +8,7 @@ class Exits(Verb):
         exits_names = [exit.name for exit in self.session.user.room.exits if not exit.hidden()]
 
         if exits_names:
-            out_message = 'Fácilmente distingues las siguientes salidas:\n  ' + '\n'.join(exits_names)
+            out_message = 'Fácilmente distingues las siguientes salidas:\n  ' + '\n  '.join(exits_names)
         else:
             out_message = 'No hay ninguna manera obvia de salir de esta habitación.'
 
