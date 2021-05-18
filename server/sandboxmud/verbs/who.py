139a95bc9def0d1ebc6b69db4aa0074606bed7e5
oliverlsanz <oliverlsanz@gmail.com>
2021-05-18 13:21:33 +0200

    mend

--- server/sandboxmud/verbs/who.py
+++ server/sandboxmud/verbs/who.py
@@ -10,15 +10,20 @@ class Who(verb.Verb):
 
     def process(self, message):
         out_message = self.get_player_list()
-        self.session.send_to_room(out_message)
+        self.session.send_to_client(out_message)
         self.finish_interaction()
 
     def get_player_list(self):
         connected_users = entities.User.objects(client_id__ne=None)
-        list_rows = [f'  {user.name}\n' for user in connected_users]
+        list_rows = [f'  {user.name: <26}  - En {self.get_location(user)}\n' for user in connected_users]
         users_list = ''.join(list_rows)
-        out = textwrap.dedent(f"""
-            Jugadores conectados:
-            {users_list}"""
-        )
+        out = f"Jugadores conectados:\n{users_list}"
         return out
+
+    def get_location(self, user):
+        if user.room is None:
+            return 'el lobby'
+        world = user.room.world_state.get_world()
+        if not world.public and not world in self.session.user.joined_worlds:
+            return 'un mundo privado'
+        return world.name
