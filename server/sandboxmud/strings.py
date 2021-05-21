79a521923d3ac81a451bc7732d49a6cabb2f20d0
oliverlsanz <oliverlsanz@gmail.com>
2021-05-21 16:27:53 +0200

    add strings module

--- server/sandboxmud/strings.py
+++ server/sandboxmud/strings.py
@@ -1,2 +1,11 @@
-many_found = _("There is more than one thing with a similar name. Be more specific.")
-not_found = _("You can't find that here.")
+many_found        = _("There is more than one thing with a similar name. Be more specific.")
+not_found         = _("You can't find that here.")
+cancelled         = _("Action cancelled")
+cancel_prompt     = _('❌ You can send "/" anytime to cancel.')
+is_empty         = _("It can't be empty.")
+
+
+def format(title, body, cancel=False):
+    subtitle_bar = '─'*len(title)
+    cancel = cancel_prompt + '\n\n' if cancel else ''
+    return f'{title}\n{subtitle_bar}\n{cancel}{body}'
