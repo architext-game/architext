ba50b2a3310fc16a928d997370f64f563f796a06
oliverlsanz <oliverlsanz@gmail.com>
2021-05-25 16:00:54 +0200

    merge WorldEditPermissions into EditWorld verb

--- server/sandboxmud/verbs/privileges.py
+++ server/sandboxmud/verbs/privileges.py
@@ -1,35 +1,6 @@
 from . import verb
 from .. import entities
 
-class ChangeEditFreedom(verb.Verb):
-    command = 'cambiarprivilegios'
-    permissions = verb.CREATOR
-
-    def process(self, message):
-        message = 'Quién quieres que pueda editar el mundo?\n  0. Todos los usuarios\n  1. Solo tu y los editores a los que des permisos'
-        self.session.send_to_client(message)
-        self.process = self.process_chosen_option
-
-    def process_chosen_option(self, message):
-        try:
-            chosen_option = int(message)
-        except ValueError:
-            self.session.send_to_client("Introduce un número")
-            return
-
-        world = self.session.user.room.world_state.get_world()
-
-        if chosen_option == 0:
-            world.set_to_free_edition()
-            self.session.send_to_client("Hecho, ahora todos pueden editar.")
-            self.finish_interaction()
-        elif chosen_option == 1:
-            world.set_to_privileged_edition()
-            self.session.send_to_client("Hecho, ahora solo tú y tus editores pueden editar.")
-            self.finish_interaction()
-        else:
-            self.session.send_to_client('Introduce un número correspondiente a una de las opciones.')
-
 
 class MakeEditor(verb.Verb):
     command = 'hacereditor '
