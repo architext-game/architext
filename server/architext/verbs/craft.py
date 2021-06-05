f3fab4823f78432352ccc1d46f9cd1c14a823316
OliverLSanz <oliverlsanz@gmail.com>
2021-06-06 01:56:01 +0200

    add craftsaved verb - resolves #143

--- server/architext/verbs/craft.py
+++ server/architext/verbs/craft.py
@@ -6,13 +6,15 @@ import architext.strings as strings
 class Craft(verb.Verb):
     """This verb allows users to create items that are placed in their current room"""
 
-    command = _('craft')
+    command      = _('craft')
+    save_command = _('craftsaved')
     permissions = verb.PRIVILEGED
 
     def __init__(self, session):
         super().__init__(session)
         self.new_item = entities.Item(room=self.session.user.room, save_on_creation=False)
         self.current_process_function = self.process_first_message
+        self.save_create = False  # if true, the item will be saved and not placed in the room.
 
     def process(self, message):
         if message == '/':
@@ -22,8 +24,19 @@ class Craft(verb.Verb):
             self.current_process_function(message)
 
     def process_first_message(self, message):
-        title = _("You start crafting an item")
-        body = _("Enter the following fields\n ⚑ Item's name")
+        if message == self.save_command:
+            self.save_create = True
+            title = _("You are save-crafting an item.")
+            body = _(
+                "It won't be created at this room but as a saved item that you'll be able to spawn later.\n"
+                "\n"
+                "Enter the following fields\n"
+                " ⚑ Item's name"
+            )
+        else:
+            title = _("You start crafting an item")
+            body = _("Enter the following fields\n ⚑ Item's name")
+        
         out_message = strings.format(title, body, cancel=True)
         self.session.send_to_client(out_message)
         self.current_process_function = self.process_item_name
@@ -67,12 +80,18 @@ class Craft(verb.Verb):
             self.session.send_to_client(_('Answer "listed", "visible", "hidden" or "takable.'))
             return
 
+        if self.save_create:
+            self.new_item.saved_in = self.session.user.room.world_state
+            self.new_item.item_id = self.new_item._generate_item_id()
+            self.new_item.room = None
+
         try:
             self.new_item.save()
         except entities.NameNotGloballyUnique:
             self.session.send_to_client(_("There is an item or exit with that name in this world. Takable items need an unique name. Choose another visibility or start over to choose another name."))
         else:
-            self.new_item.put_in_room(self.session.user.room)
+            if not self.save_create:
+                self.new_item.put_in_room(self.session.user.room)
             self.session.send_to_client(_("Item crafted!"))
             if not self.session.user.master_mode:
                 self.session.send_to_others_in_room(_("{user_name} has crafted something here.").format(user_name=self.session.user.name))
