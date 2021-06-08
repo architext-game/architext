fc0e3c78029c29c958179e0919ded9dd0e183f0c
oliverlsanz <oliverlsanz@gmail.com>
2021-06-08 13:17:55 +0200

    add wrapping to sent text messages

--- server/architext/session.py
+++ server/architext/session.py
@@ -3,6 +3,7 @@
 from . import entities
 from . import verbs as v
 from . import util
+import textwrap
 
 class Session:
     """This class handles interaction with a single user, though it can send messages to other users as well, to inform them of the session's user actions.
@@ -67,35 +68,47 @@ class Session:
             self.user.disconnect()
 
     def send_to_client(self, message):
-        self.server.send_message(self.client_id, "\n\r"+message)
+        self.send(self.client_id, "\n\r"+message)
         if self.logger:
             self.logger.info('server\n'+message)
 
     def send_to_user(self, user, message):
         if user.client_id is not None:
-            self.server.send_message(user.client_id, "\n\r"+message)
+            self.send(user.client_id, "\n\r"+message)
 
     def send_to_room_except(self, exception_user, message):
         users_in_this_room = entities.User.objects(room=self.user.room)
         for user in users_in_this_room:
             if user != exception_user:
-                self.server.send_message(user.client_id, message)
+                self.send(user.client_id, message)
 
     def send_to_others_in_room(self, message):
         self.send_to_room_except(self.user, message)
-        """users_in_this_room = entities.User.objects(room=self.user.room)
-        for user in users_in_this_room:
-            if user != self.user:
-                self.server.send_message(user.client_id, message)"""
 
     def send_to_room(self, message):
         users_in_this_room = entities.User.objects(room=self.user.room)
         for user in users_in_this_room:
-            self.server.send_message(user.client_id, message)
+            self.send(user.client_id, message)
 
     def send_to_all(self, message):
         for user in entities.User.objects:
-            self.server.send_message(user.client_id, message)
+            self.send(user.client_id, message)
+
+    def send(self, client_id, message):
+        # Wrap the message to 80 characters
+        message = '\n'.join(
+            # Wrap each line individually 
+            ['\n'.join(textwrap.wrap(line, 80, 
+                replace_whitespace=False,
+                expand_tabs=False,
+                drop_whitespace=False,
+                break_on_hyphens=False,
+                break_long_words=False
+                ))
+            for line in message.splitlines()]
+        )
+            
+        self.server.send_message(client_id, message)
 
     def set_logger(self, logger):
         self.logger = logger
