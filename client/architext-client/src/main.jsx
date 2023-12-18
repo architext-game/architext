0e7aee17123ddb10123b5ae8f6bbe52b22dd7964
Oliver L. Sanz <oliverlsanz@gmail.com>
2023-12-18 13:30:10 +0100

    feat: add web app meta tag

--- client/architext-client/src/main.jsx
+++ client/architext-client/src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="mobile-web-app-capable" content="yes" />
     <App />
   </React.StrictMode>,
 )
