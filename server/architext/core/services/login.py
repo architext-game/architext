bed540e19d4eccfbd1257a4df6a198fec0bbd661
OliverLSanz <oliverlsanz@gmail.com>
2024-12-26 00:56:40 +0100

    server: add error message on bad login

--- server/architext/core/services/login.py
+++ server/architext/core/services/login.py
@@ -4,7 +4,8 @@ from architext.core.commands import Login, LoginResult
 def login(uow: UnitOfWork, command: Login, client_user_id: str = "") -> LoginResult:
     with uow:
         user = uow.users.get_user_by_email(command.email)
-        assert user is not None
+        if user is None:
+            raise ValueError("User does not exist")
         success = user.match_password(command.password)
         if not success:
             raise ValueError("Password or email are not correct")
