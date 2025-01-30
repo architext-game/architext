c69ad24e447d246f0f7d833e5004b6eb74250794
OliverLSanz <oliverlsanz@gmail.com>
2025-01-30 13:13:32 +0100

    web: add icon

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -24,6 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
