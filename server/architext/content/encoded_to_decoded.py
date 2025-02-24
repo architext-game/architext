528fda8cae6b98517f96cf4c1887b41cb9274d0b
OliverLSanz <oliverlsanz@gmail.com>
2025-02-24 09:58:08 +0100

    content: create tutorial world

---
+++ server/architext/content/encoded_to_decoded.py
@@ -0,0 +1,8 @@
+from architext.content.new_tutorial import NEW_TUTORIAL_ENCODED
+from architext.core.handlers.import_world import decode_text
+import pprint
+import json
+
+text = NEW_TUTORIAL_ENCODED
+
+print(json.dumps(json.loads(decode_text(text)), indent=2, ensure_ascii=False))
